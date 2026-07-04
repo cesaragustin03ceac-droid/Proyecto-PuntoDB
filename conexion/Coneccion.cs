e11572f44a35ddc888b4fe68abc8ae8f80d7de7b
Cesar Agustin <cesar.agustin03.ceac@gmail.com>
2026-07-04 16:10:52 -0600

    creacion de la conexion

---
+++ conexion/Coneccion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Punto.conexion
+{
+    internal class Coneccion
+    {
+        private readonly string cadena;
+
+        public Coneccion()
+        {
+            cadena = "Server=127.0.0.1; Database=puntodb; Uid=root; Pwd=; Port=3306";
+
+        }
+
+
+        public MySqlConnection obtenerconexion()
+        {
+            try
+            {
+                MySqlConnection conexion = new MySqlConnection(cadena);
+                conexion.Open();
+                MessageBox.Show("Conectado corrextamente...");
+                return conexion;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error al conectar la base de datos " + ex.ToString());
+                return null;
+            }
+        }
+    }
+}
